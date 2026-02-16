143be3528d3200f8039b2dc07b0e22f1d165c5f3
lingxiaotian <w646645452@gmail.com>
2026-02-16 20:59:01 +0800

    Update PromptHub to 0.4.0 with correct SHA256

--- Casks/prompthub.rb
+++ Casks/prompthub.rb
@@ -2,12 +2,12 @@ cask "prompthub" do
   version "0.4.0"
 
   on_arm do
-    sha256 "PLACEHOLDER_ARM64_SHA256"
+    sha256 "0ff78d34ac10acd42386e3caec6d16bd9effbc2a99091d14638dbe900e243296"
     url "https://github.com/legeling/PromptHub/releases/download/v#{version}/PromptHub-#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "PLACEHOLDER_X64_SHA256"
+    sha256 "4a19abb6a24d32c06b7554020385682aa2f7e2595b05f7bb1d787c04bf47fbac"
     url "https://github.com/legeling/PromptHub/releases/download/v#{version}/PromptHub-#{version}-x64.dmg"
   end
 
